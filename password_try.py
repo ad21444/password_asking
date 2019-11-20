34a0fef04b112dc2d0d10db7bcc9acf8108d1159
Edward Hsu <ad21444@yahoo.com.tw>
2019-11-20 19:00:25 +0800

    add 0 no show

--- password_try.py
+++ password_try.py
@@ -3,12 +3,14 @@ trytime = 3
 
 while trytime>0:
 	pwd = input('請輸入密碼')
-	print(pwd)
 	if pwd == password:
 		print('登入成功')
-		trytime = 4
+		trytime = 4 #讓while不成立
 		break
 	else:
 		trytime = trytime-1
-		print('密碼錯誤，還有', trytime ,'次機會' )
+		if trytime == 0:
+			print('沒機會了')
+		else:
+			print('密碼錯誤，還有', trytime ,'次機會' )
 
